093165abf1b9d8dff854c8974908aeccb0abfbab
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-11-17 20:19:03 +0900

    [test] 자동배포 테스트

--- src/main/java/sopt35/skyscanner/controller/TestController.java
+++ src/main/java/sopt35/skyscanner/controller/TestController.java
@@ -1,4 +1,15 @@
 package sopt35.skyscanner.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "SUCCESS!";
+    }
 }
