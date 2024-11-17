ac79aaf2f0bf7cb476f4ae428f0b84db1eda4b70
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-11-17 11:40:44 +0900

    [INIT] FIRST COMMIT

---
+++ src/main/java/sopt35/skyscanner/SkyscannerApplication.java
@@ -0,0 +1,13 @@
+package sopt35.skyscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyscannerApplication.class, args);
+	}
+
+}
