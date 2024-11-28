1dfd4b7ac4a8ff2940a9227b68ddca3f23c2a597
Hoyoung027 <hybyun0207@gmail.com>
2024-11-28 22:04:05 +0900

    [FIX] 캘린더 호출 API 색 반환 추가

--- src/main/java/sopt35/skyscanner/dto/PriceResponse.java
+++ src/main/java/sopt35/skyscanner/dto/PriceResponse.java
@@ -7,11 +7,13 @@ public class PriceResponse {
     private long id;
     private LocalDate date;
     private Float price;
+    private String color;
 
     public PriceResponse(long id, LocalDate date, Float price) {
         this.id = id;
         this.date = date;
         this.price = price;
+        this.color = determineColor(price);
     }
 
     public long getId() {
@@ -26,6 +28,10 @@ public class PriceResponse {
         return date;
     }
 
+    public String getColor() {
+        return color;
+    }
+
     public void setId(long id) {
         this.id = id;
     }
@@ -36,5 +42,28 @@ public class PriceResponse {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // 가격에 따라 색상을 결정하는 메서드
+    private String determineColor(Float price) {
+
+        float RedThreshold = 32.95F;
+        float OrangeThreshold = 17.58F;
+
+        if(price == null){
+            return "None";
+        }
+
+        if (price >= RedThreshold) { // 32.95만원 이상
+            return "r";
+        } else if (price >= OrangeThreshold) { // 17.58만원 이상
+            return "o";
+        } else { // 17.58만원 미만
+            return "g";
+        }
     }
 }
