f7ffb3e804e00249bd9638e214f417b42975be42
Hoyoung027 <hybyun0207@gmail.com>
2024-11-30 03:03:05 +0900

    [FIX] 프론트 도메인의 요청 허용

--- src/main/java/sopt35/skyscanner/config/WebConfig.java
+++ src/main/java/sopt35/skyscanner/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 특정 도메인에서만 CORS 요청을 허용
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // 허용할 출처 : 특정 도메인만 받을 수 있음
+                .allowedOrigins("http://localhost:5173", "https://35-collaboration-web-skyscanner-6mls.vercel.app") // 허용할 출처 : 특정 도메인만 받을 수 있음
                 .allowedMethods("GET", "POST", "PATCH") // 허용할 HTTP method
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With") // 클라이언트가 보낼 수 있는 헤더들
                 .exposedHeaders("Authorization") // 클라이언트가 응답 헤더에서 읽을 수 있는 헤더들
